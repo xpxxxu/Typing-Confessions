e11afb04c7b97053435337e5ef56dce925032245
HUSSAIN <xpxxxu@gmail.com>
2025-10-16 12:42:08 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Replace with your Firebase project config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
